fcfcd9020ceeffb5baca499fbb392190963479f0
Altamash Shahid <altamashshahid93@gmail.com>
2024-10-14 12:40:27 +0530

    Update Associative_array.sv

--- Associative_array.sv
+++ Associative_array.sv
@@ -4,7 +4,8 @@
 3.Element of an array can be of any type we can store the concatenation of various data type or class structure as well.
 4.it is used when we use "large memory" of the order of Giga Bytes.
     syntax: data_type array[index_type];
-    note:- Index type of associative array can be "string", "bit" ,"int" only.
+    note:- Index type of associative array can be "string", "bit" ,"int",enum only.
     note:- If index type is not integer then in foreach or for loop it will iterate alphabatically.
+5.Associative arrays are excellent for creating lookup tables or mapping values.For instance, you can use them to store configuration settings, error codes 
 */
 
