c4bd034421cf2d4ed2123516aa433f352dcab76d
Altamash Shahid <altamashshahid93@gmail.com>
2024-11-06 10:08:02 +0530

    Update Encapsulation_&_Datahiding.sv

--- oops/Encapsulation_&_Datahiding.sv
+++ oops/Encapsulation_&_Datahiding.sv
@@ -8,58 +8,43 @@ This avoids class member modification outside the class scope and its misuse.
 -->Public: these members can  be accessed by any extended class
 
 */
-///////////////   local   /////////////
-class local_encapsulation;
-  bit [31:0] packet;
-  local int parity;
+//////////////////////////////////////////////////////
+class parent_trans;
+    local logic[1:0]a;
+    protected logic[1:0]b;
 
-  function void display();
-    $display("packet = %0d and parity = %0d", packet,parity);
-  endfunction
+    protected function void display();
+        $display("a=%0d, b=%0d",a,b);
+    endfunction
+
+    function void update (logic [1:0]a,b);
+        this.a=a;
+        this.b=b;
+    endfunction
 endclass
 
-module class_example;
-  local_encapulation tr;
-  
-  initial begin
-    tr = new();
-   
-    tr.packet = 100;
-    tr.parity = 0;
+class child_trans extends parents_trans;
+    logic[1:0]c;
     
-    tr.display();
-  end
-endmodule
-/////////////   private    ///////////////
-class transaction;
-  bit [31:0] data;
-  int id;
+    function void calc();
+        super.display();
+        super.a=2;
+        super.b=1;
+    endfunction
 
-  function new();
-    data = 100;
-    id = 1;
-  endfunction
-  
-  protected function void display();
-    $display("data = %0d and id = %0d", data, id);
-  endfunction
 endclass
+ module test;
+     parent_trans par_h;
+     child_trans c_h;
+     par_h=new();
 
-class child_trans extends transaction;
-  bit [31:0] addr;
-  
-  task calc_addr;
-    addr = data * id;
-    display();  // child class is accessing protected access method of parent class
-    $display("addr = %0d", addr);
-  endtask
-endclass
+     par_h.a=2;
+     par_h.b=2;
 
-module class_example;
-  child_trans tr;
-  
-  initial begin
-    tr = new();
-    tr.calc_addr();
-  end
-endmodule
+     par_h.update(1,2);
+     c_h=new();
+     c_h.update(2,3);
+     c_h.display();
+     c_h.calc();
+     end
+ endmodule
