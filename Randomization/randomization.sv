0455a4829af521763b5f5d960bacbf47719f9106
Altamash Shahid <altamashshahid93@gmail.com>
2025-04-29 16:38:34 +0530

    Update randomization.sv
    simple randomizaton of two variable

--- Randomization/randomization.sv
+++ Randomization/randomization.sv
@@ -1,1 +1,19 @@
+class test;
+  rand bit [15:0] a;
+  rand bit [3:0] b;
 
+endclass
+
+module top;
+  test t;
+  initial
+    begin
+      t=new;
+      t.randomize();
+      $display("randomize value of a=%0d b=%0d",t.a,t.b);;
+
+    end
+  end
+endmodule
+  
+  
