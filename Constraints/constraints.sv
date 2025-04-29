0649db60ea58985ad99f03b1a3bc9c476af412fb
Altamash Shahid <altamashshahid93@gmail.com>
2025-04-29 17:02:16 +0530

    Update constraints.sv

--- Constraints/constraints.sv
+++ Constraints/constraints.sv
@@ -1,1 +1,5 @@
-
+/*
+1.constraint restrict the random data generation to meaning values.
+2.constraint are declarative and bidirectional code
+syntax- constraint_identifier{constraiint expression[];}
+*/
