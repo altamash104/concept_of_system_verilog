226af149ca0e891d15f690683ff196c21d095b1d
Altamash Shahid <altamashshahid93@gmail.com>
2025-04-29 17:14:52 +0530

    Update constraints.sv

--- Constraints/constraints.sv
+++ Constraints/constraints.sv
@@ -3,3 +3,22 @@
 2.constraint are declarative and bidirectional code
 syntax- constraint_identifier{constraiint expression[];}
 */
+class test;
+  rand bit [3:0]a;
+  rand bit [3:0]b;
+  
+  constraint c1{a>5;}
+  constraint c2{a<10;}
+endclass
+
+module top;
+  test t;
+  initial
+    begin
+      t=new;
+      t.randomize();
+      $display("value of a=%0d, b=%0d",t.a,t.b);
+
+    end
+endmodule
+  
