528e5c76f38f0f148e8cd0bb13368eb9daced3e7
Altamash Shahid <altamashshahid93@gmail.com>
2025-05-01 12:01:50 +0530

    Update casting.sv

--- oops/casting.sv
+++ oops/casting.sv
@@ -1,6 +1,8 @@
 /*
-Static casting:In case of static casting we do not check the compability.
+1.Static casting:In case of static casting we do not check the compability.
 here we change one data type to anoher data type.
+2.static cast truncates the least significant bits of the source value.
+3.
 
       syntax:  '(value, variable,expression)
 dynamic casting: In case of dynamic casting we check compatibility before making illegal assignment in to legal assignment.
@@ -9,7 +11,46 @@ it can be use das function and task.
       synatax- $cast(destination ,source);
       note: data type of both source and destination shoulb be same.
 */
+///////////////////static casting////////////////////
+module top;
+  
+  int i_num1;
+  real r_num2;
+  
+  initial
+    begin
+      i_num1= int'(10.0-1.2);
+      $display("the integer value of i_num1=%0d",i_num1);
+          r_num2= real'(5/2);// here we are dividing integer by integer so we will get real'(2)
+      $display("the integer value of i_num1=%0.3f",r_num2);
+               
+    end
+endmodule
 
+/*
+the integer value of i_num1=9
+the integer value of i_num1=2.000 //static casting only do type conversion ,it does not preserve value.
+*/
+///////////////////////to get floating value////////////////////
+module top;
+  
+  int i_num1;
+  real r_num2;
+  
+  initial
+    begin
+      i_num1= int'(10.0-1.2);
+      $display("the integer value of i_num1=%0d",i_num1);
+          r_num2= real'((5.0)/2);// here we are dividing real by integer so we will get real'(2)
+      $display("the integer value of i_num1=%0.3f",r_num2);
+               
+    end
+endmodule
+
+/*
+the integer value of i_num1=9
+the integer value of i_num1=2.500 //static casting only do type conversion ,it does not preserve value.
+*/
 class parent;
     virtual task print();
         $display("calling from parent class");
