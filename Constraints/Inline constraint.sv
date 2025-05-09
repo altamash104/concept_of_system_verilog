91916e7e57492dcfb1a45bc9e8bb06904961d9a4
Altamash Shahid <altamashshahid93@gmail.com>
2025-05-09 10:08:38 +0530

    Update Inline constraint.sv

--- Constraints/Inline constraint.sv
+++ Constraints/Inline constraint.sv
@@ -1,15 +1,16 @@
 /*
 1.It is used restrict the range of randomization.
-2.Class constraint are permanent constraint.we can disable it sice they are named constraint.
-3.Inline constraint temporary constraint.
+2.Class constraint are permanent constraint.we can disable it since they are named constraint.
+3.Inline constraint is a temporary constraint.
 4.These contraint are "temporary or one time " in nature used with randomization function directly without any name to it.
 5.Since it is not a named constraint it can not be disabled.
-6.Inline constraint will also override class constraint.
+6.Inline constraint can not  override hard constraint written in class.
 */
 
 class trans;
   rand bit [7:0] data;
   constraint data_range { data inside {[0:255]};}
+  //constraint data_range {data>110;} //this is a hard constraint can not be overriden by inline constraint resulting randomization failure. 
 endclass
 
 module top;
@@ -17,7 +18,7 @@ trans t_h;
   initial
     begin
       repeat(10)begin
-	      trans t_h = new();
+	trans t_h = new();
         t_h.randomize() with { data < 100; }; // data 0 to 99 only for this call
         $display("data=%0d",t_h.data);
       end
